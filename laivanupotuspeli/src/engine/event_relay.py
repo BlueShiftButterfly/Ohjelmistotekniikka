42a4050c9a9f8b48462f0b82633da1d86cfd6177
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-11-19 23:16:49 +0200

    Added unsubscribe method to event relay

--- laivanupotuspeli/src/engine/event_relay.py
+++ laivanupotuspeli/src/engine/event_relay.py
@@ -2,14 +2,21 @@ from engine.event import Event
 
 class EventRelay:
     def __init__(self):
-        self.subscribers:dict[Event, list] = {}
+        self.subscribers:dict[Event, dict] = {}
 
     def subscribe(self, object, func, event: Event):
-        if (event not in self.subscribers.keys()):
-            self.subscribers[event] = []
-        self.subscribers[event].append( tuple([object, func]) )
+        if event not in self.subscribers.keys():
+            self.subscribers[event] = {}
+        self.subscribers[event][func] = object
+
+    def unsubscribe(self, func, event: Event):
+        if event not in self.subscribers.keys():
+            return
+        if func not in self.subscribers[event].keys():
+            return
+        del self.subscribers[event][func]
 
     def call(self, event: Event):
         if event not in self.subscribers.keys(): return
-        for sub in self.subscribers[event]:
-            sub[1](sub[0])
+        for func, obj in self.subscribers[event].items():
+            func(obj)
