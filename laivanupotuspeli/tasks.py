35cdb0c97417e8b8145825d0ef1957fff81b1857
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-12-18 21:52:33 +0200

    Added os check for poetry invoke tasks

--- laivanupotuspeli/tasks.py
+++ laivanupotuspeli/tasks.py
@@ -1,21 +1,28 @@
+import os
 from invoke import task
+
+def use_pty():
+    if os.name == "nt":
+        return False
+    else:
+        return True
 
 @task
 def start(ctx):
-    ctx.run("python src/index.py", pty=True)
+    ctx.run("python src/index.py", pty=use_pty())
 
 @task
 def test(ctx):
-    ctx.run("pytest src", pty=True)
+    ctx.run("pytest src", pty=use_pty())
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=use_pty())
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
+    ctx.run("coverage html", pty=use_pty())
 
 @task
 def lint(ctx):
-    ctx.run("pylint src", pty=True)
+    ctx.run("pylint src", pty=use_pty())
